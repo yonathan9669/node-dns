6ceaf59d9ac4986c69cb575f6e289a77d5545168
Yonathan Morales <yonathanmorales@gmail.com>
2017-02-16 17:57:40 -0400

    Structure changed to allow multiple domains and records be updated at once on the same account.
    The recordId isn't required anymore all data is now been retrieve for easy configuration.
    
    This is a simple and first implementation that should be improved.

--- app.js
+++ app.js
@@ -1,95 +1,124 @@
-var fs = require("fs"),
-    appPath = require("path").dirname(process.argv[1]),
-    Lookup = require(appPath + "/lib/lookup"),
-    DNSimple = require(appPath + "/lib/dnsimple");
+var fs = require('fs'),
+	appPath = require('path').dirname(process.argv[1]),
+	Lookup = require(appPath + '/lib/lookup'),
+	DNSimple = require(appPath + '/lib/dnsimple'),
+	moment = require('moment');
 
-var settings = JSON.parse(fs.readFileSync(appPath + "/settings.json"));
-
+var settings = JSON.parse(fs.readFileSync(appPath + '/settings.json'));
 var lookup = new Lookup(settings.currentIP);
 var dns = new DNSimple(settings);
+writeln('App Directory: ' + appPath);
 
-writeln("App Directory: " + appPath);
+lookup.on('update', function(public_ip){
+  // the update event is fired when there is a mismatch of
+  // IP addresses
+  writeln('IP address mismatch:');
+  writeln('\tCurrent IP: ' + settings.currentIP);
+  writeln('\t Public IP: ' + public_ip);
 
-// the update event is fired when there is a mismatch of
-// IP addresses
-lookup.on('update', function(public_ip) {
-    writeln("IP address mismatch:");
-    writeln("\tCurrent IP: " + settings.currentIP);
-    writeln("\t Public IP: " + public_ip);
-    writeln("Updating record...");
-
-    dns.update(public_ip);
-})
-// the match event is called when the current and the public
-// IP addresses match
-    .on('match', function (current_ip, public_id) {
-        writeln("IP addresses match.  No need to update.");
-    });
-
-// the updated event is raised after the public IP
-// was successfully updated
-dns.on('updated', function(data) {
-    writeln("Domain IP updated successfully.");
-
-    var parentRecord = JSON.parse(data);
-    settings.currentIP = parentRecord.record.content;
-
-    writeln("Updating local settings...");
-
-    // we have to save the new public IP to a local file
-    // so that we can do a comparison on the next run
-    fs.writeFile(appPath + "/settings.json", JSON.stringify(settings, undefined, 4), function (err, fd) {
-        if (err) throw err;
-    });
-
-    writeln("Local settings updated successfully.");
-
-}).on('error', function(e) {
-    writeln("Error: " + e);
+  updateRecords(public_ip);
+}).on('match', function(){
+  // the match event is called when the current and the public
+  // IP addresses match
+  writeln('IP addresses match.  No need to update.');
 });
 
-//lookup.compare();
+dns.on('record-list', function(domain, records){
+  // the update event is fired when there is a mismatch of
+  // IP addresses
+  domain.records = domain.records.map(function(record){
+	var r = records.find(function(apiRecord){
+		  return apiRecord.name === record.name;
+		}) || {};
 
-// Uncomment the below to run continuously
-//  current set to run once an hour
+	return {
+	  name: record.name,
+	  content: r.content,
+	  id: r.id,
+	  ttl: r.ttl
+	}
+  });
 
-checkIpStatus();
-setInterval(checkIpStatus, settings.checkInterval);
+  if (!--updated) {
+	updateConfigFile(function(){
+	  checkIpStatus();
+	});
+  }
+}).on('record-updated', function(record, domain){
+  // the updated event is raised after the public IP
+  // was successfully updated
+  writeln('On ' + domain.name + ' the ' + record.name + ' record IP was updated successfully.');
 
-function checkIpStatus() {
-    printDate();
-    lookup.compare();
+  if (!--updated) {
+	settings.currentIP = record.content;
+	writeln('All records were successfully updated...');
+	updateConfigFile();
+  }
+}).on('error', function(e){
+  console.error(e);
+});
+
+function updateConfigFile(cb){
+  // we have to save the new public IP to a local file
+  // so that we can do a comparison on the next run
+
+  writeln('Updating local settings...');
+  fs.writeFile(appPath + '/settings.json', JSON.stringify(settings, undefined, 2), function(err){
+	if (err) throw err;
+	writeln('Local settings updated successfully.');
+	cb && cb();
+  });
 }
 
-function writeln(str) {
-    process.stdout.write(str + "\n");
+function checkIpStatus(){
+  printDate();
+  lookup.compare(settings.currentIP);
 }
 
-function printDate() {
-    writeln("//------------------------------------------------------//");
-    writeln("//----------------" + getDateTime() + "-----------------//");
+function updateRecords(newIp){
+  writeln('Updating records...');
+  updated = 0;
+  settings.domains.forEach(function(domain){
+	updated += domain.records.length;
+
+	domain.records.forEach(function(record){
+	  if (record.id && record.content !== newIp) {
+		dns.update(domain, record, newIp);
+	  }
+	  else {
+		updated--;
+	  }
+	});
+  });
+
+  if (!updated) {
+	writeln('All records were successfully updated...');
+  }
 }
 
-function getDateTime() {
-
-    var date = new Date();
-
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-
-    var min = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-
-    var sec = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-
-    var year = date.getFullYear();
-
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-
-    var day = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-
-    return year + "/" + month + "/" + day + " - " + hour + ":" + min + ":" + sec;
+function writeln(str){
+  process.stdout.write(str + '\n');
 }
+
+function printDate(){
+  writeln('//----------------' + moment().format('MMMM Do YYYY, h:mm:ss a') + '-----------------//');
+}
+
+var domains = settings.domains.filter(function(domain){
+  return domain.records.some(function(record){
+	return !record.id;
+  });
+});
+
+var updated = domains.length;
+
+if (!updated) {
+  checkIpStatus();
+} else {
+  settings.currentIP = "0.0.0.0";
+  domains.forEach(function(domain){
+	dns.list(domain);
+  });
+}
+
+setInterval(checkIpStatus, settings.checkInterval || 3600000);
